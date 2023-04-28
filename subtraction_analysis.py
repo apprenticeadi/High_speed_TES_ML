cd5e68ff32c1857e3267e28e6be29796abeaaf4a
Zhenghao Li <zhenghao.li.1999@gmail.com>
2023-04-28 13:08:40 +0100

    new method for shifting high frequency data

--- subtraction_analysis.py
+++ subtraction_analysis.py
@@ -179,123 +179,133 @@ heights_600, bins_600, _ = plt.hist(overlaps_600, bins=1000)
 midBins_600 = (bins_600[1:] + bins_600[:-1]) / 2
 multiplier = 0.6
 hist_fit = fitting_histogram(heights_600, midBins_600, overlaps_600, multiplier)
-lower_list, upper_list = hist_fit.fitting(figname = f'fit {frequency}kHz raw')
+lower_list, upper_list = hist_fit.fitting(figname=f'fit {frequency}kHz raw')
 binning_index_600, binning_traces_600 = hist_fit.trace_bin(data_600)
-# #%%
+# %%
 '''
 shift the data such that the zero photon data have mean overlap=0
 '''
 mean_0 = np.mean(binning_traces_600[0], axis=0)  # voltage mean value of zero photon traces
 sig_0 = np.sqrt(np.var(binning_traces_600[0], axis=0))
-plt.figure(f'{frequency}kHz traces mean and std')
-plt.plot(mean_0)
-plt.plot(sig_0)
+plt.figure(f'{frequency}kHz 0-photon traces mean and std')
+plt.plot(mean_0, label='mean')
+plt.plot(sig_0, label='std')
+plt.legend()
 offset = min(mean_0)
 print(offset)
 
 data_shifted = np.array(data_600) - offset
 ave_trace_shifted = ave_trace_600 - offset
-binning_traces_shifted = [ [data_shifted[index] for index in index_list]  for index_list in binning_index_600]
-#%%
-overlap_list_shifted = [ np.dot(ave_trace_shifted,amplitude)  for amplitude in data_shifted]  # calculate overlap between shifted traces and shifted mean trace
-heights_shifted,bins_shifted,_=plt.hist(overlap_list_shifted,bins=1000)
 
-midBins_shifted= (bins_shifted[1:] + bins_shifted[:-1])/2
+# binning_traces_shifted = {}
+# for photon_number in binning_traces_600.keys():
+#     binning_traces_shifted[photon_number] = binning_traces_600[photon_number] - offset
+
+# %%p
+overlap_list_shifted = ave_trace_shifted @ data_shifted.T
+# overlap_list_shifted_2 = [ np.dot(ave_trace_shifted,amplitude)  for amplitude in data_shifted]  # calculate overlap between shifted traces and shifted mean trace
+plt.figure(f'{frequency}kHz shifted stegosaurus')
+heights_shifted, bins_shifted, _ = plt.hist(overlap_list_shifted, bins=1000)
+
+'''
+Fit the shifted data but only the 1-photon peak is important here. 
+'''
+midBins_shifted = (bins_shifted[1:] + bins_shifted[:-1]) / 2
 multiplier = 0.6
 hist_fit = fitting_histogram(heights_shifted, midBins_shifted, overlap_list_shifted, multiplier)
-lower_list, upper_list = hist_fit.fitting(figname = f'fit {frequency}kHz shifted')
+lower_list, upper_list = hist_fit.fitting(figname=f'fit {frequency}kHz shifted')
 binning_index_600, binning_traces_600 = hist_fit.trace_bin(data_shifted)
 
-mean_trace_600 = [np.mean(traces,axis=0)  for traces in binning_traces_600]
+mean_trace_600 = [np.mean(traces, axis=0) for traces in binning_traces_600]
 plt.figure(f'{frequency}kHz average shifted trace for each photon number')
-for mean in mean_trace_600 :
+for mean in mean_trace_600:
     plt.plot(mean)
 plt.ylabel('voltage')
 plt.xlabel('time (in sample)')
 plt.title('average trace for each photon numbers')
 # plt.show()
-#%%
-'''mapping the mean traces from 100kHz data to 600kHz data'''
-# =============================================================================
-# max_mean_500 = [max(mean)  for mean in mean_trace_500]
-# argmax_mean_500 = [np.argmax(mean)  for mean in mean_trace_500]
-# max_mean_100 = [max(mean)  for mean in mean_trace_100]
-# mean_trace_100_pad = [ np.insert(mean,0,list(mean[-40:]))  for mean in mean_trace_100]
-# argmax_mean_100 = [np.argmax(mean)  for mean in mean_trace_100_pad]
-# =============================================================================
-mean_trace_100_pad = [ np.insert(mean,0,list(mean[-40:]))  for mean in mean_trace_100.values()]   # pad 40 samples in front of the data to avoid negative index in tail subtraction
-
-diff_max = max(mean_trace_600[1]) / max(mean_trace_100[1])
-diff_arg = np.argmax(mean_trace_100_pad[1]) - np.argmax(mean_trace_600[1])  # the position of the peak to map the time range of 100khz to that of 600khz
-
-#mean_scaled_100 = [ mean_trace_100_pad[i][(argmax_mean_100[i]-argmax_mean_500[i]) :] /max_mean_100[i] * max_mean_500[i]    for i in range(1,8)]
-mean_scaled_100 = [ mean_trace_100_pad[i][diff_arg:]*diff_max     for i in range(1,9)]  # The pad ensures that the max in 100kHz always come later than the max in 600kHz
-mean_scaled_100.insert( 0, mean_trace_100[0] )  # no need to scale zero photon traces.
-
-plt.figure('Mean trace for two frequencies')
-plt.plot(mean_scaled_100[0],color='black',label='100kHz trace(after scaling)')
-plt.plot(mean_trace_600[0],color='red',label='600kHz trace')
-for i in range(1,len(mean_trace_600)):
-    plt.plot(mean_scaled_100[i],color='black')
-    plt.plot(mean_trace_600[i],color='red')
-plt.ylabel('voltage')
-plt.xlabel('time (in sample)')
-plt.title('average trace for each photon numbers')
-plt.legend()
-# plt.show()
-#%%
-'''
-tail subtraction
-'''
-subtract=0
-mean_max = [max(mean)  for mean in mean_trace_100_pad]
-mean_argmax = [np.argmax(mean)  for mean in mean_trace_100_pad]
-
-mean_trace_period = [mean_scaled_100[i][:period] for i in range(len(mean_scaled_100))]
-#mean_trace_2period = [mean_scaled_100[i][period:2*period] for i in range(len(mean_scaled_100))]
-def mean_trace_subtraction(index,subtract):
-    trace = np.array(data_shifted[index]) - subtract
-    diff = [  np.mean( abs(trace - mean_trace_period[i]) )   for i in range(len(mean_scaled_100))]
-    PN = np.argmin(diff)  # photon number correspond to this trace
-    if PN ==0:
-        subtract = 0
-        fit=np.zeros(period)
-    else:
-        trace_max = max(trace[:60])
-        trace_argmax = np.argmax(trace[:60])
-
-        offset_diff = mean_argmax[PN] - trace_argmax
-
-        fit = mean_trace_100_pad[PN][ offset_diff :] / mean_max[PN] * trace_max
-        subtract = fit[period:2*period]
-
-
-# =============================================================================
-#     plt.plot(data_shifted[index])
-#     plt.plot(fit[:period],color='red')
-#     plt.plot(trace)
-#     plt.show()
-# =============================================================================
-    return trace,subtract
-
-#number_store=[0]
-subtracted_trace=[]
-for i in range(len(data_shifted)):
-    trace,subtract = mean_trace_subtraction(i,subtract)
-    subtracted_trace.append(trace)
-#%%
-ave_subtracted = np.mean(subtracted_trace,axis=0)
-plt.figure('Mean trace after tail subtraction')
-plt.plot(ave_subtracted)
-plt.ylabel('voltage')
-plt.xlabel('time (in sample)')
-plt.title('mean trace after tail subtraction')
-# plt.show()
-#%%
-plt.figure('histogram after subtraction')
-overlap = [ np.dot(ave_subtracted,amplitude)  for amplitude in subtracted_trace]
-plt.hist(overlap,bins=1000)
-plt.ylabel('frequencies')
-plt.xlabel('overlap')
-plt.title('historgams after subtraction')
-# plt.show()
+# %%
+# '''mapping the mean traces from 100kHz data to 600kHz data'''
+# # =============================================================================
+# # max_mean_500 = [max(mean)  for mean in mean_trace_500]
+# # argmax_mean_500 = [np.argmax(mean)  for mean in mean_trace_500]
+# # max_mean_100 = [max(mean)  for mean in mean_trace_100]
+# # mean_trace_100_pad = [ np.insert(mean,0,list(mean[-40:]))  for mean in mean_trace_100]
+# # argmax_mean_100 = [np.argmax(mean)  for mean in mean_trace_100_pad]
+# # =============================================================================
+# mean_trace_100_pad = [ np.insert(mean,0,list(mean[-40:]))  for mean in mean_trace_100.values()]   # pad 40 samples in front of the data to avoid negative index in tail subtraction
+#
+# diff_max = max(mean_trace_600[1]) / max(mean_trace_100[1])
+# diff_arg = np.argmax(mean_trace_100_pad[1]) - np.argmax(mean_trace_600[1])  # the position of the peak to map the time range of 100khz to that of 600khz
+#
+# #mean_scaled_100 = [ mean_trace_100_pad[i][(argmax_mean_100[i]-argmax_mean_500[i]) :] /max_mean_100[i] * max_mean_500[i]    for i in range(1,8)]
+# mean_scaled_100 = [ mean_trace_100_pad[i][diff_arg:]*diff_max     for i in range(1,9)]  # The pad ensures that the max in 100kHz always come later than the max in 600kHz
+# mean_scaled_100.insert( 0, mean_trace_100[0] )  # no need to scale zero photon traces.
+#
+# plt.figure('Mean trace for two frequencies')
+# plt.plot(mean_scaled_100[0],color='black',label='100kHz trace(after scaling)')
+# plt.plot(mean_trace_600[0],color='red',label='600kHz trace')
+# for i in range(1,len(mean_trace_600)):
+#     plt.plot(mean_scaled_100[i],color='black')
+#     plt.plot(mean_trace_600[i],color='red')
+# plt.ylabel('voltage')
+# plt.xlabel('time (in sample)')
+# plt.title('average trace for each photon numbers')
+# plt.legend()
+# # plt.show()
+# #%%
+# '''
+# tail subtraction
+# '''
+# subtract=0
+# mean_max = [max(mean)  for mean in mean_trace_100_pad]
+# mean_argmax = [np.argmax(mean)  for mean in mean_trace_100_pad]
+#
+# mean_trace_period = [mean_scaled_100[i][:period] for i in range(len(mean_scaled_100))]
+# #mean_trace_2period = [mean_scaled_100[i][period:2*period] for i in range(len(mean_scaled_100))]
+# def mean_trace_subtraction(index,subtract):
+#     trace = np.array(data_shifted[index]) - subtract
+#     diff = [  np.mean( abs(trace - mean_trace_period[i]) )   for i in range(len(mean_scaled_100))]
+#     PN = np.argmin(diff)  # photon number correspond to this trace
+#     if PN ==0:
+#         subtract = 0
+#         fit=np.zeros(period)
+#     else:
+#         trace_max = max(trace[:60])
+#         trace_argmax = np.argmax(trace[:60])
+#
+#         offset_diff = mean_argmax[PN] - trace_argmax
+#
+#         fit = mean_trace_100_pad[PN][ offset_diff :] / mean_max[PN] * trace_max
+#         subtract = fit[period:2*period]
+#
+#
+# # =============================================================================
+# #     plt.plot(data_shifted[index])
+# #     plt.plot(fit[:period],color='red')
+# #     plt.plot(trace)
+# #     plt.show()
+# # =============================================================================
+#     return trace,subtract
+#
+# #number_store=[0]
+# subtracted_trace=[]
+# for i in range(len(data_shifted)):
+#     trace,subtract = mean_trace_subtraction(i,subtract)
+#     subtracted_trace.append(trace)
+# #%%
+# ave_subtracted = np.mean(subtracted_trace,axis=0)
+# plt.figure('Mean trace after tail subtraction')
+# plt.plot(ave_subtracted)
+# plt.ylabel('voltage')
+# plt.xlabel('time (in sample)')
+# plt.title('mean trace after tail subtraction')
+# # plt.show()
+# #%%
+# plt.figure('histogram after subtraction')
+# overlap = [ np.dot(ave_subtracted,amplitude)  for amplitude in subtracted_trace]
+# plt.hist(overlap,bins=1000)
+# plt.ylabel('frequencies')
+# plt.xlabel('overlap')
+# plt.title('historgams after subtraction')
+# # plt.show()
