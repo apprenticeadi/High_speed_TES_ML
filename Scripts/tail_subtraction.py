7eabdae2f422011c6aac221cb8e7a482321ada5f
Zhenghao Li <zhenghao.li.1999@gmail.com>
2023-05-03 18:21:20 +0100

    put tail subtraction script into functions

--- Scripts/tail_subtraction.py
+++ Scripts/tail_subtraction.py
@@ -1,79 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import os
 
-from fitting_hist import fitting_histogram
-from utils import read_high_freq_data, read_raw_data
+from src.utils import read_high_freq_data, read_raw_data
+from src.traces import Traces
 
 # This script is built on Ruidi's original subtraction_analysis script
 # %%
 
+multiplier = 0.6
+num_bins = 1000
+
 '''Calibration data'''
-data_100_ = read_raw_data(100)
-data_100 = data_100_.T
+data_100 = read_raw_data(100)
+calibration_traces = Traces(frequency=100, data=data_100, multiplier=multiplier, num_bins=num_bins)
+offset_cal, _ = calibration_traces.subtract_offset()
 # %%
 
 '''higher frequency data'''
-frequency = 800
+frequency = 600
 data_high = read_high_freq_data(frequency)
-period = int(5e4 / frequency)
+target_traces = Traces(frequency=frequency, data=data_high, multiplier=multiplier, num_bins=num_bins)
+offset_target, _ = target_traces.subtract_offset()  # shift the data such that the characteristic 0 photon trace has mean 0
+freq_str = target_traces.freq_str
 
 '''
-Analysis for 100kHz, to find the characteristic shape of the waveform for each photon number
+Analysis for 100kHz, to find the characteristic trace for each photon number
 '''
-min_voltage = np.amin(data_100)
-max_voltage = np.amax(data_100)
-ymin = 5000 * (min_voltage // 5000)
-ymax = 5000 * (max_voltage // 5000 + 1)
 
-
-'''
-plot first _ traces in 100kHZ data
-'''
-n_traces_100 = 100
-
-plt.figure(f'first {n_traces_100} 100kHz traces')
-for i in range(n_traces_100):
-    plt.plot(data_100[i][:200])
-plt.ylabel('voltage')
-plt.xlabel('time (in sample)')
-plt.xlim(0, 200)
-plt.ylim(ymin, ymax)
+calibration_traces.plot_traces(50)
+cal_hist_fit = calibration_traces.fit_histogram(plot=True)  # fit stegosaurus for calibration data
+cal_char_traces = calibration_traces.characteristic_traces_pn(plot=True)  # find characteristic trace for each photon number
 
 '''
-plot average trace
+Analysis for higher frequency data
 '''
-ave_trace = np.mean(data_100, axis=0)
-#
-offset = min(ave_trace)  # zero photons should give zero voltage.
-data_100 -= offset
-
-plt.figure('average trace 100kHz')
-ave_trace = np.mean(data_100, axis=0)
-plt.plot(ave_trace)
-plt.ylabel('voltage')
-plt.xlabel('time (in sample)')
-plt.ylim(ymin, ymax)
-plt.title('average trace')
-
-
-'''
-Plot first x traces of high rep rate data
-'''
-num_high_traces = 100
-plt.figure(f'First {num_high_traces} traces of {frequency}kHz')
-for i in range(num_high_traces):
-    plt.plot(data_high[i])
-plt.xlim(0, period)
-# plt.show()
-# # %%
-
-'''
-Plot average trace of high rep rate data
-'''
-plt.figure(f'average {frequency}kHz trace')
-ave_trace_high = np.mean(data_high, axis=0)
-plt.plot(ave_trace_high)
-# plt.show()
-# # %%
+target_traces.plot_traces(50)
+tar_hist_fit = target_traces.fit_histogram(plot=True)
+tar_char_traces = target_traces.characteristic_traces_pn(plot=True)
 
