0fa7dd45735c72a0529216edd4818b075e14f7a4
Zhenghao Li <zhenghao.li.1999@gmail.com>
2023-06-20 14:48:17 +0100

    Merge pull request #3 from apprenticeadi/development
    Development

--- src/ML_funcs.py
+++ src/ML_funcs.py
@@ -2,6 +2,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, accuracy_score
+import xgboost as xgb
 
 class ML:
 
@@ -15,12 +16,17 @@ class ML:
 
         if self.modeltype == 'RF':
             self.classifier = RandomForestClassifier()
-        if self.modeltype == 'SVM':
-            self.classifier = SVC()
-        if self.modeltype!= 'RF' and self.modeltype!= 'SVM':
-            raise Exception('modeltype must be 'RF' (random forest) or ''SVM'' (support vector machines)')
 
-    def makemodel(self):
+        elif self.modeltype == 'SVM':
+            self.classifier = SVC()
+
+        elif self.modeltype == 'BDT':
+            self.dtrain = xgb.DMatrix(self.x_train, label=self.y_train)
+            self.dtest = xgb.DMatrix(self.x_test, label=self.y_test)
+        else:
+            raise Exception('modeltype must be "RF", "SVM" or  "BDT" (Random forest, support vector machines or boosted decision tree)')
+
+    def makemodel(self, num_rounds = 10):
 
         if self.modeltype == 'RF':
             x_train, x_test, y_train, y_test = train_test_split(self.dataset, self.labels)
@@ -28,21 +34,31 @@ class ML:
         if self.modeltype == 'SVM':
             x_train, x_test, y_train, y_test = train_test_split(self.dataset, self.labels)
             self.classifier.fit(x_train, y_train)
+        if self.modeltype == 'BDT':
+            params = {
+                'max_depth': 3,
+                'eta': 0.1,
+                'num_class': 10,
+                'objective': 'multi:softmax'
+            }
+            self.classifier = xgb.train(params, self.dtrain, num_rounds)
+
 
     def accuracy_score(self):
-        if self.modeltype == 'RF':
-            rf_predictions = self.classifier.predict(self.x_test)
-            rf_accuracy = accuracy_score(self.y_test, rf_predictions)
-            return rf_accuracy
 
-        if self.modeltype == 'SVM':
-            svm_predictions = self.classifier.predict(self.x_test)
-            svm_accuracy = accuracy_score(self.y_test, svm_predictions)
-            return svm_accuracy
+            predictions = self.classifier.predict(self.x_test)
+            accuracy = accuracy_score(self.y_test, predictions)
+            return accuracy
+
 
     def predict(self, targets):
-        predictions = self.classifier.predict(targets)
-        return predictions
+        if self.modeltype == 'BDT':
+            d_matrix = xgb.DMatrix(targets)
+            predictions = self.classifier.predict(d_matrix)
+            return predictions.astype(int)
+        else:
+            predictions = self.classifier.predict(targets)
+            return predictions
 
 
 
