9605b2c7fde2933f6ebac57a6e162f10d8a53458
Matthew Kendall <mattkendall321@gmail.com>
2023-06-23 16:56:35 +0100

    23/06/23 added shift and density plots

--- Scripts/Overlap_ML.py
+++ Scripts/Overlap_ML.py
@@ -17,9 +17,7 @@ calibrationTraces = Traces(frequency=100, data=data_100, multiplier=multiplier, 
 # frequency = 900
 # data_high = DataUtils.read_high_freq_data(frequency)  # unshifted
 # targetTraces = Traces(frequency=frequency, data=data_high, multiplier=multiplier, num_bins=num_bins)
-#
 # tar_ave_trace, tar_ave_trace_stdp, tar_ave_trace_stdm = targetTraces.average_trace(plot=False)
-# cal_chars = calibrationTraces.characteristic_traces_pn(plot=False)
 # shifted_cal_traces = TraceUtils.shift_trace(tar_ave_trace, calibrationTraces, pad_length=guess_peak*2, id=1)
 
 
@@ -44,48 +42,33 @@ filtered_label = np.delete(labels, filtered_ind)
 filtered_data = Traces(100, filtered_traces)
 
 frequency = 500
-# freq_values =[ 500,600,700,800,900]
-# fig, axs = plt.subplots(nrows=3, ncols=2, figsize=(15, 12))
+freq_values =[ 500,600,700,800,900]
+fig, axs = plt.subplots(nrows=3, ncols=2, figsize=(15, 12))
+plt.suptitle("Multiplier = 1", fontsize=14)
+for frequency, ax in zip(freq_values, axs.ravel()):
 
-# for frequency, ax in zip(freq_values, axs.ravel()):
-#     data_high = filtered_data.overlap_to_high_freq(frequency)
-#     model = ML(data_high, filtered_label, modeltype='SVM')
-#     model.makemodel(num_rounds=25)
-#     actual_data = DataUtils.read_high_freq_data(frequency)
-#     targetTraces = Traces(frequency=frequency, data=actual_data, multiplier=multiplier, num_bins=num_bins)
-#     offset_target, _ = targetTraces.subtract_offset()
-#     actual_data = actual_data - offset_target * 0.7
-#     if pca_cleanup:
-#         actualTraces = Traces(frequency=frequency, data=actual_data)
-#         actual_data = actualTraces.pca_cleanup(num_components=pca_components)
-#
-#     test = model.predict((actual_data))
-#     ax.bar(list(range(len(np.bincount(test)))), np.bincount(test))
-#     ax.set_title(str(frequency)+ 'kHz accuracy score = '+str(model.accuracy_score())[0:5])
-# plt.show()
+    data_high = filtered_data.overlap_to_high_freq(frequency)
+    model = ML(data_high, filtered_label, modeltype='BDT')
+    model.makemodel(num_rounds=25)
+    actual_data = DataUtils.read_high_freq_data(frequency)
+    targetTraces = Traces(frequency=frequency, data=actual_data, multiplier=multiplier, num_bins=num_bins)
+    offset_target, _ = targetTraces.subtract_offset()
+    actual_data = actual_data - offset_target
+    if pca_cleanup:
+        actualTraces = Traces(frequency=frequency, data=actual_data)
+        actual_data = actualTraces.pca_cleanup(num_components=pca_components)
 
+    test = model.predict((actual_data))
+    pn = test[100]
+    indices = np.where(filtered_label == pn)[0]
+    pn_data = data_high[indices]
+    for x in pn_data:
+        ax.plot(x,color = 'r', linestyle = 'dashed', linewidth = 0.1)
+    #for x in data_high:
+        #ax.plot(x,color = 'r', linestyle = 'dashed', linewidth = 0.2)
+    ax.plot(actual_data[100],color = 'k', label = 'signal')
+    # ax.bar(list(range(len(np.bincount(test)))), np.bincount(test))
+    # ax.set_title(str(frequency)+ 'kHz accuracy score = '+str(model.accuracy_score())[0:5])
+    ax.set_title(str(frequency) + ' kHz, classifacation: ' + str(pn))
 
-# actual_data = DataUtils.read_high_freq_data(frequency)
-# targetTraces = Traces(frequency=frequency, data=actual_data, multiplier=multiplier, num_bins=num_bins)
-# freq_str = targetTraces.freq_str
-#
-# '''Shift data'''
-# # I'm actually not sure if I should do this or not... It might give the traces an incorrect height.
-# offset_target, _ = targetTraces.subtract_offset()
-# actual_data = actual_data - offset_target*0.7
-#
-# data_high = filtered_data.overlap_to_high_freq(frequency)
-# calibrationTraces.characteristic_traces_pn(plot=True)
-# num = 8
-# plt.plot(actual_data[num], label = 'signal', linewidth = 3)
-# model = ML(data_high, filtered_label, modeltype='SVM')
-# model.makemodel()
-# pred = model.predict(actual_data)
-# print(pred[num])
-# plt.legend()
-# plt.show()
-data_high = filtered_data.overlap_to_high_freq(frequency)
-for i in range(20):
-    plt.plot(data_high[i], label = filtered_label[i])
-plt.legend()
 plt.show()
