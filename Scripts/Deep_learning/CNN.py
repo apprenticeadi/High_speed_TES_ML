2d33d5b10bfeeee422ae37bdbeac1840711e660b
mkendall11 <mattkendall321@gmail.com>
2023-08-20 15:35:47 +0100

    CNN edits

--- Scripts/Deep_learning/CNN.py
+++ Scripts/Deep_learning/CNN.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Conv1D, MaxPooling1D, Flatten, Dense, Dropout, LSTM
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from src.utils import DataUtils
 from src.traces import Traces
 import tensorflow as tf
@@ -12,7 +13,7 @@ from sklearn.model_selection import StratifiedKFold
 from keras.utils import to_categorical
 
 
-time_series, labels = return_artifical_data(600,3)
+time_series, labels = return_artifical_data(600,1.5,8)
 # Initialize k-fold cross-validation
 skf = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
 
@@ -25,7 +26,7 @@ test_losses = []
 X_train, X_test, y_train, y_test = train_test_split(time_series, labels, test_size=0.2, random_state=42)
 
 # Convert labels to categorical format
-num_classes = 10
+num_classes = 11
 y_train = to_categorical(y_train, num_classes)
 y_test = to_categorical(y_test, num_classes)
 print(time_series.shape)
@@ -58,19 +59,16 @@ model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy
 model.fit(X_train, y_train, batch_size=8, epochs=13, validation_data=(X_test, y_test))
 
 # Evaluate the model
-accuracy = model.evaluate(X_test, y_test)[1]
-print("Test Accuracy:", accuracy)
+y_pred_prob = model.predict(X_test)
+y_pred = np.argmax(y_pred_prob, axis=1)
 
-actual_data = DataUtils.read_high_freq_data(600,2, new = True)
-targetTraces = Traces(frequency=600, data=actual_data, multiplier=3, num_bins=1000)
-offset_target = 0.5*targetTraces.return_av_diff()
-actual_data = actual_data - offset_target
-actual_data = np.reshape(actual_data, (actual_data.shape[0], actual_data.shape[1], 1))
-predictions = model.predict(actual_data)
-predicted_class = np.argmax(predictions, axis = 1)
+accuracy = accuracy_score(np.argmax(y_test, axis=1), y_pred)
+precision = precision_score(np.argmax(y_test, axis=1), y_pred, average='weighted')
+recall = recall_score(np.argmax(y_test, axis=1), y_pred, average='weighted')
+f1 = f1_score(np.argmax(y_test, axis=1), y_pred, average='weighted')
 
-x_vals = list(range(len(np.bincount(predicted_class))))
-y_vals = np.bincount(predicted_class)
-
-plt.bar(x_vals, y_vals)
-plt.show()
+print("Metrics for RNN Model:")
+print(f"Accuracy: {accuracy:.4f}")
+print(f"Precision: {precision:.4f}")
+print(f"Recall: {recall:.4f}")
+print(f"F1-Score: {f1:.4f}")
--- Scripts/test.py
+++ Scripts/test.py
@@ -1,70 +1,74 @@
-import numpy as np
 import matplotlib.pyplot as plt
-from src.utils import DataUtils, TraceUtils
-from src.traces import Traces
-from sklearn.feature_selection import SelectKBest,mutual_info_classif, f_classif
-from src.ML_funcs import ML, return_artifical_data, extract_features
+import numpy as np
 from sklearn.model_selection import train_test_split
-from scipy.signal import find_peaks
-from tqdm.auto import tqdm
-from scipy.stats import skew, kurtosis
-from scipy.signal import welch
-import pandas as pd
-power = 6
-frequency = 400
-data, labels = return_artifical_data(frequency,1.5,power)
-peak_data = []
-'''
-calculate features for each time-series
-'''
-for series in tqdm(data):
-    feature = extract_features(series)
-    peak_data.append(feature)
+from keras.models import Sequential
+from keras.layers import Conv1D, MaxPooling1D, Flatten, Dense, Dropout, LSTM
+from src.utils import DataUtils
+from src.traces import Traces
+import tensorflow as tf
+import numpy as np
+from src.ML_funcs import return_artifical_data
+from sklearn.model_selection import StratifiedKFold
+from keras.utils import to_categorical
 
-features = np.array(peak_data)
-'''
-train model on time series
-'''
-model = ML(features, labels, modeltype='RF')
-model.makemodel()
-print(model.accuracy_score())
-'''
-find most important features
-'''
-selector = SelectKBest(score_func=mutual_info_classif, k=9)
-X_train, X_test, y_train, y_test = train_test_split(features, labels)
-X_train_selected = selector.fit_transform(X_train, y_train)
-X_test_selected = selector.transform(X_test)
-important_feature_ind = selector.get_support(indices=True)
-print("Indices of Important Features:", important_feature_ind)
 
-# Print F-scores of all features
-all_feature_scores = selector.scores_
-for idx, score in enumerate(all_feature_scores):
-    print(f"Feature {idx}: Score = {score}")
-'''
-load in actual data
-'''
-actual_data = DataUtils.read_high_freq_data(frequency,power,new = True)
-actual_trace = Traces(frequency,actual_data, 1.8,1000)
-#offset, _ = actual_trace.subtract_offset()
-actual_data = actual_data - 2000
-'''
-extract features for actual data
-'''
-extracted_features = []
-for time_series in tqdm(actual_data):
-    features = extract_features(time_series)
-    extracted_features.append(features)
+time_series, labels = return_artifical_data(600,1.5,8)
+# Initialize k-fold cross-validation
+skf = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
 
-'''
-make predicitons
-'''
-test = np.array(extracted_features)
-predictions = model.predict(test)
-y = np.bincount(predictions)
-x = range(len(y))
+# Initialize list to store Test Accuracies
+test_accs = []
 
-plt.bar(x,y)
-plt.show()
+# Initialize list to store Test Losses
+test_losses = []
+
+X_train, X_test, y_train, y_test = train_test_split(time_series, labels, test_size=0.2, random_state=42)
+
+# Convert labels to categorical format
+num_classes = 11
+y_train = to_categorical(y_train, num_classes)
+y_test = to_categorical(y_test, num_classes)
+print(time_series.shape)
+
+input_shape = (time_series.shape[1], 1)  # assuming the shape is (length, features)
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+
+length = len(X_train)
+# Define the CNN model
+# model = Sequential()
+# # model.add(LSTM(64, input_shape=input_shape))
+# # model.add(Dense(num_classes, activation='softmax'))
+# model.add(Conv1D(filters=32, kernel_size=3, activation='relu', input_shape=input_shape))
+# model.add(MaxPooling1D(pool_size=2))
+# model.add(Conv1D(filters=64, kernel_size=3, activation='relu'))
+# model.add(MaxPooling1D(pool_size=2))
+# model.add(Flatten())
+# model.add(Dense(128, activation='relu'))
+# model.add(Dropout(0.5))
+# model.add(Dense(11, activation='softmax'))
+model = tf.keras.Sequential([
+    tf.keras.layers.LSTM(units=32, return_sequences=True, input_shape=(length, 1)),
+    tf.keras.layers.LSTM(units=16),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(num_classes, activation='softmax')
+])
+
+
+# Compile the model
+opt = tf.keras.optimizers.Adam(learning_rate=0.001)
+model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+
+# Train the model
+model.fit(X_train, y_train, batch_size=8, epochs=13, validation_data=(X_test, y_test))
+
+# Evaluate the model
+accuracy = model.evaluate(X_test, y_test)[1]
+print("Test Accuracy:", accuracy)
+
+
+
+
+
+
 
