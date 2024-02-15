2db2e5ef6559399bce38e3aed455cb89db9514f4
Li <zhenghao.li.1999@gmail.com>
2024-02-15 21:03:06 +0000

    improve results by refining 100khz treatment

--- Scripts/Machine_Learning/Overlap_ML.py
+++ Scripts/Machine_Learning/Overlap_ML.py
@@ -7,26 +7,34 @@ import time
 import logging
 import pandas as pd
 
-from src.utils import DataUtils, LogUtils, DFUtils
+from src.utils import LogUtils, DFUtils
+from src.data_parser import DataParser
 from src.traces import Traces
-from src.ML_funcs import ML, return_artifical_data, extract_features, find_offset
+from src.ML_funcs import ML, extract_features #return_artifical_data, find_offset
 
 '''
 script to produce  PN distributions using tabular classifiers, specify power, whether FE and modeltype.
 '''
 #specify parameters
-power = 5
+data_dir = 'RawData'
+sub_power_name = 'raw_7'
 feature_extraction = False
 modeltype = 'RF'
-multiplier = 2
-guess_mean_pn = 1.5
+multiplier = 1.
+num_bins = 500
+guess_mean_pn = 6.
+
+dataParser = DataParser(sub_dir=sub_power_name, parent_dir=data_dir)
+
 
 time_stamp = datetime.datetime.now().strftime("%Y-%m-%d(%H-%M-%S.%f)")
-results_dir = rf'..\..\Results\{modeltype}_ML_raw_{power}_{time_stamp}'
+results_dir = rf'..\..\Results\{modeltype}\{sub_power_name}_{time_stamp}'
 
 LogUtils.log_config(time_stamp='', dir=results_dir, filehead='log', module_name='', level=logging.INFO)
-logging.info(f'Produce PN distributions using tabular classifiers. Raw data from raw_{power}, feature extraction is '
-             f'{feature_extraction}, model type is {modeltype}. For inner product method at 100kHz, multiplier={multiplier}')
+logging.info(rf'Produce PN distributions using tabular classifiers. Raw data from {data_dir}\{sub_power_name}, ' 
+             rf'feature extraction is {feature_extraction}, model type is {modeltype}. '
+             rf'Inner product method used to identify calibration data at 100kHz with multiplier={multiplier} and num_bins={num_bins}. '
+             rf'The 100kHz calibration data is used to generate training data by overlapping them to higher frequencies.')
 
 max_depth = 15
 n_estimators = 600
@@ -46,27 +54,32 @@ def poisson_norm(x, mu):
 '''
 generate PN dist for 100kHz using overlap
 '''
-data100 = DataUtils.read_raw_data_new(100,power)
-trace100 = Traces(100 , data100, multiplier)
+data100 = dataParser.parse_data(100, interpolated=False, triggered=False)
+calTraces = Traces(100, data100, multiplier, num_bins=num_bins)
 
-x100,y = trace100.pn_bar_plot(plot = True, plt_title='100kHz bar plot')
-plt.savefig(DFUtils.create_filename(results_dir + r'\100kHz_inner_prod_pn.pdf'))
+calTraces.fit_histogram(plot=True)
+plt.savefig(DFUtils.create_filename(results_dir + rf'\100kHz_fit_stegosaurus.pdf'))
 
-dist100 = y/np.sum(y)
+t1 = time.time()
+_, counts = calTraces.pn_bar_plot(plot = True, plt_title='100kHz bar plot')
+t2 = time.time()
+cal_time = t2 -t1
 
-fit, cov = curve_fit(poisson_norm, x100, dist100, p0 = [guess_mean_pn]) #, maxfev=2000)
+logging.info(rf'{cal_time}s to produce photon number plot for 100kHz data')
+
+plt.savefig(DFUtils.create_filename(results_dir + fr'\100kHz_inner_prod_pn.pdf'))
+
+pns, ref_distrib = calTraces.pn_bar_plot(plot=True, plt_title='100kHz photon number distribution', normalised=True)
+
+fit, cov = curve_fit(poisson_norm, pns, ref_distrib, p0 = [guess_mean_pn]) #, maxfev=2000)
 lam = fit[0]
 lam_var = cov[0,0]
 
-plt.figure('100kHz norm bar plot')
-plt.bar(x100, dist100)
-
-dense_x = np.linspace(min(x100), max(x100), 100)
-plt.plot(dense_x, poisson_norm(dense_x, lam), color='red', label=rf'Poisson fit with $\mu={{{lam:.2f}}}$')
+plt.plot(pns, poisson_norm(pns, lam), color='red', marker='x', linestyle='solid', label=rf'$\mu={{{lam:.2f}}}$')
 
 plt.xlabel('Photon number')
 plt.ylabel('Probability')
-plt.xticks(x100[::2])
+plt.xticks(pns[::2])
 plt.legend()
 plt.title('100kHz normalised bar plot')
 plt.savefig(results_dir + r'\100kHz_inner_prod_pn_norm.pdf')
@@ -86,15 +99,14 @@ fig, axs = plt.subplots(nrows=3, ncols=3, figsize=(15, 12))
 # probabilities[0, :] = dist100
 
 # results file by me, stored in results
-results_df = pd.DataFrame(np.zeros((len(freq_values)+1, 4 + len(dist100))),
-                          columns=['rep_rate', 'acc_score', 'fit_mu', 'fit_var'] + list(x100))
-results_df.loc[0] = [100, np.nan, fit[0], cov[0,0]] + list(dist100)
+results_df = pd.DataFrame(columns=['rep_rate', 'acc_score', 'fit_mu', 'fit_var', 'training_t', 'predict_t'] + list(pns))
+results_df.loc[0] = [100, np.nan, lam, lam_var, np.nan, cal_time] + list(ref_distrib)
 
 i_freq = 0
 for frequency,ax in zip(freq_values, axs.ravel()):
     i_freq = i_freq + 1
-    data_high, labels = return_artifical_data(frequency, multiplier, power)
-    features = data_high
+    training_data, training_labels = calTraces.generate_training_data(frequency)
+    features = training_data
 
     if feature_extraction:
 
@@ -103,7 +115,7 @@ for frequency,ax in zip(freq_values, axs.ravel()):
         '''
 
         peak_data = []
-        for series in data_high:
+        for series in training_data:
             feature = extract_features(series)
             peak_data.append(feature)
 
@@ -114,17 +126,27 @@ for frequency,ax in zip(freq_values, axs.ravel()):
     '''
     logging.info(f'Start building model for {frequency}kHz with artificial data')
     t1= time.time()
-    model = ML(features, labels, modeltype=modeltype, max_depth=max_depth, n_estimators=n_estimators)
+    model = ML(features, training_labels, modeltype=modeltype, max_depth=max_depth, n_estimators=n_estimators)
     model.makemodel()
     t2 = time.time()
+
+    training_t = t2 -t1
     accuracy = model.accuracy_score()  # accuracy score of the test artificial samples (25% by default)
-    logging.info(f'Model complete after {t2-t1}s. Test samples (25%) achieve accuracy score={accuracy}')
+    logging.info(f'Model complete after {training_t}s. Test samples (25%) achieve accuracy score={accuracy}')
 
     '''
     load in real data
     '''
-    actual_data = DataUtils.read_high_freq_data(frequency, power= power, new= True)
-    shift = find_offset(frequency, power)
+    actual_data = dataParser.parse_data(frequency, interpolated=False, triggered=False) # DataUtils.read_high_freq_data(frequency, power= power, new= True)
+
+    '''
+    Correct for vertical shift
+    '''
+    # TODO: this is terrible. Where does the huge shift in average trace come from? Maybe from the way training data is generated?
+    av_training = np.mean(training_data, axis=0)
+    av_actual = np.mean(actual_data, axis=0)
+
+    shift = np.max(av_actual) - np.max(av_training)
     actual_data = actual_data - shift
     actual_features = actual_data
 
@@ -139,11 +161,15 @@ for frequency,ax in zip(freq_values, axs.ravel()):
     '''
     make predictions
     '''
-
+    #TODO: time this
+    t1 = time.time()
     predictions = model.predict((actual_features))
+    t2 = time.time()
+    predict_t = t2 - t1
+    logging.info(f'Actual data predicted after {predict_t}s.')
 
     #
-    y_vals = np.bincount(predictions, minlength=len(x100))
+    y_vals = np.bincount(predictions, minlength=len(pns))
     y_vals = y_vals / np.sum(y_vals)
     x_vals = np.arange(len(y_vals))
 
@@ -154,10 +180,9 @@ for frequency,ax in zip(freq_values, axs.ravel()):
     fit data
     '''
     fit, cov = curve_fit(poisson_norm, x_vals, y_vals, p0=[guess_mean_pn], maxfev = 2000)
-    dense_x_vals = np.linspace(0, max(x_vals), 100)
-    ax.plot(dense_x_vals, poisson_norm(dense_x_vals, fit[0]), label=rf'Poisson fit with $\mu={{{fit[0]:.2f}}}$', color='r')
+    ax.plot(x_vals, poisson_norm(x_vals, fit[0]), label=rf'$\mu={{{fit[0]:.2f}}}$', color='r')
 
-    ax.set_title(f'{frequency}kHz accuracy score = {accuracy:.4f}')
+    ax.set_title(f'{frequency}kHz')
     ax.legend()
     ax.set_xticks(x_vals[::2])
 
@@ -165,10 +190,10 @@ for frequency,ax in zip(freq_values, axs.ravel()):
     Save results  
     '''
     # probabilities[i_freq] = y_vals
-    results_df.loc[i_freq] = [frequency, accuracy, fit[0], cov[0,0]] + list(y_vals)
-    results_df.to_csv(results_dir + rf'\{modeltype}_probs_raw{power}.csv', index=False)
+    results_df.loc[i_freq] = [frequency, accuracy, fit[0], cov[0,0], training_t, predict_t] + list(y_vals)
+    results_df.to_csv(results_dir + rf'\{modeltype}_results_{sub_power_name}.csv', index=False)
 
-    plt.savefig(results_dir + rf'\{modeltype}_probs_raw{power}.pdf')
+    plt.savefig(results_dir + rf'\{modeltype}_results_{sub_power_name}.pdf')
 # plt.show()
 
 # '''
@@ -182,5 +207,5 @@ for frequency,ax in zip(freq_values, axs.ravel()):
 '''
 save probabilities for tomography
 '''
-results_df.to_csv(DFUtils.create_filename(rf'..\..\Params\{modeltype}_probs_raw{power}.csv'), index=False)
+# results_df.to_csv(DFUtils.create_filename(rf'..\..\Params\{modeltype}_probs_raw{power}.csv'), index=False)
 
