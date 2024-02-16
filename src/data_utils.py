83621a164efef88cfe65fc4a7401b7a95fe3c031
Li <zhenghao.li.1999@gmail.com>
2024-02-16 12:08:20 +0000

    reformate data utilities

--- src/data_utils.py
+++ src/data_utils.py
@@ -67,6 +67,63 @@ class DataParser(object):
 
         return data_raw
 
+    def parse_data(self, frequency, interpolated=False, triggered=True, **kwargs):
+        """
+        Return data array, where each row is a trace.
+
+        :param frequency: Rep rate, unit: kHz
+        :param interpolated: If interpolated, then 500 samples per trace. Otherwise, int(5e4/frequency) samples per trace.
+        :param triggered: The first trace is triggered on the rising edge.
+        :param kwargs: Extra arguments in read_raw_data.
+        """
+
+        data_raw = self.read_raw_data(frequency, **kwargs)
+
+        # interpolate the data, such that each trace has 500 sample points
+        f = frequency // 100
+        ex_samples, data_intp = DataCleaner.interpolate_data(data_raw, f)
+
+        if triggered:
+            if frequency < 300:
+                # No triggering for lower frequency yet.
+                # TODO: fix this.
+                warnings.warn(f'No triggering method for {frequency}kHz yet')
+                trigger = 0
+
+            else:
+                triggers = DataCleaner.find_trigger(data_intp, samples_per_trace=500)
+                trigger = int(np.median(triggers))
+
+        else:
+            trigger = 0
+
+        if interpolated:
+            intp_traces = DataCleaner.chop_traces(data_intp, samples_per_trace=500, trigger=trigger)
+            return intp_traces
+
+        else:
+            # give back the un-interpolated data.
+            samples_per_trace = int(5e4 / frequency)
+            intp_samples = DataCleaner.chop_traces(ex_samples, samples_per_trace=500, trigger=trigger)
+
+            num_rows = len(data_raw)
+            num_traces_per_row = len(intp_samples)
+
+            traces = np.zeros((num_rows, samples_per_trace * num_traces_per_row))
+
+            for i, s in enumerate(intp_samples):
+                integer_s = np.arange(ceil(s[0]), int(s[-1]) + 1)
+                integer_s = integer_s[:samples_per_trace]  # trim to the same length
+
+                traces[:, i * samples_per_trace : (i+1) * samples_per_trace] = data_raw[:, integer_s]
+
+            traces = traces.reshape((num_rows * num_traces_per_row, samples_per_trace))
+
+            return traces
+
+
+class DataCleaner(object):
+
     @staticmethod
     def interpolate_data(data_raw, f):
         """ Interpolate the raw data by inserting (f-1) evenly spaced sample points between every two neighbouring
@@ -108,7 +165,7 @@ class DataParser(object):
         if method == 'troughs':
             for i in range(len(data)):
                 troughs, _ = find_peaks(- data[i], distance=samples_per_trace - samples_per_trace // 10)
-                triggers[i] = int(np.median(troughs[:n_troughs] % samples_per_trace))
+                triggers[i] = int(np.median(troughs[1:n_troughs+1] % samples_per_trace))
 
         else:
             raise ValueError(rf'method {method} not supported yet.')
@@ -118,60 +175,6 @@ class DataParser(object):
         else:
             return triggers
 
-    def parse_data(self, frequency, interpolated=False, triggered=True, **kwargs):
-        """
-        Return data array, where each row is a trace.
-
-        :param frequency: Rep rate, unit: kHz
-        :param interpolated: If interpolated, then 500 samples per trace. Otherwise, int(5e4/frequency) samples per trace.
-        :param triggered: The first trace is triggered on the rising edge.
-        :param kwargs: Extra arguments in read_raw_data.
-        """
-
-        data_raw = self.read_raw_data(frequency, **kwargs)
-
-        # interpolate the data, such that each trace has 500 sample points
-        f = frequency // 100
-        ex_samples, data_intp = self.interpolate_data(data_raw, f)
-
-        if triggered:
-            if frequency < 300:
-                # No triggering for lower frequency yet.
-                # TODO: fix this.
-                warnings.warn(f'No triggering method for {frequency}kHz yet')
-                trigger = 0
-
-            else:
-                triggers = self.find_trigger(data_intp, samples_per_trace=500)
-                trigger = int(np.median(triggers))
-
-        else:
-            trigger = 0
-
-        if interpolated:
-            intp_traces = self.chop_traces(data_intp, samples_per_trace=500, trigger=trigger)
-            return intp_traces
-
-        else:
-            # give back the un-interpolated data.
-            samples_per_trace = int(5e4 / frequency)
-            intp_samples = self.chop_traces(ex_samples, samples_per_trace=500, trigger=trigger)
-
-            num_rows = len(data_raw)
-            num_traces_per_row = len(intp_samples)
-
-            traces = np.zeros((num_rows, samples_per_trace * num_traces_per_row))
-
-            for i, s in enumerate(intp_samples):
-                integer_s = np.arange(ceil(s[0]), int(s[-1]) + 1)
-                integer_s = integer_s[:samples_per_trace]  # trim to the same length
-
-                traces[:, i * samples_per_trace : (i+1) * samples_per_trace] = data_raw[:, integer_s]
-
-            traces = traces.reshape((num_rows * num_traces_per_row, samples_per_trace))
-
-            return traces
-
 
 if __name__ == '__main__':
     frequency = 600
@@ -180,7 +183,7 @@ if __name__ == '__main__':
     samples = np.arange(data_raw.shape[1])
 
     f = frequency // 100
-    ex_samples, data_intp = dataParser.interpolate_data(data_raw, f)
+    ex_samples, data_intp = DataCleaner.interpolate_data(data_raw, f)
     period = ex_samples[500]
 
     import matplotlib.pyplot as plt
@@ -192,11 +195,11 @@ if __name__ == '__main__':
         ax.plot(ex_samples, data_intp[i])
     show_i = 0
     ax.set_xlim(show_i * period, (show_i + 5) * period)
-    triggers = dataParser.find_trigger(data_intp, samples_per_trace=500)
+    triggers = DataCleaner.find_trigger(data_intp, samples_per_trace=500)
     for i, ax in enumerate(axs):
         ax.axvline(ex_samples[triggers[i]], ymin=0, ymax=max(data_intp[i]), color='gray', linestyle='dashed')
 
-    raw_traces = dataParser.chop_traces(data_raw, samples_per_trace=int(5e4 // frequency), trigger=0)
+    raw_traces = DataCleaner.chop_traces(data_raw, samples_per_trace=int(5e4 // frequency), trigger=0)
     intp_traces = dataParser.parse_data(frequency, interpolated=True, triggered=False)
     traces = dataParser.parse_data(frequency, interpolated=False, triggered=True)
 
