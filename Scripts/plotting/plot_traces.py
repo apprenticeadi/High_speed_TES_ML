c818bdd0d750876b1c4ecdd1afc7fb130ee9eb9c
Li <zhenghao.li.1999@gmail.com>
2024-02-21 12:33:54 +0000

    plot for paper

--- Scripts/plotting/plot_traces.py
+++ Scripts/plotting/plot_traces.py
@@ -64,51 +64,56 @@ to_show = 1000
 data100_to_plot = data100[:2*to_show].reshape((to_show, 2*500))
 data_high_to_plot = data_high[:2*to_show].reshape((to_show, 2*period))
 
+fontsize = 14
 # plot together
-fig, axs = plt.subplot_mosaic([['(a)', '(b)'], ['(d)', '(c)']],
-                              figsize=(12, 6), height_ratios=[3,4])
-plt.subplots_adjust(hspace=0.35, wspace=0.)
+fig, axs = plt.subplot_mosaic([['(a) 100kHz', f'(b) {high_freq}kHz']],
+                              figsize=(10,3), layout='constrained')
+# plt.subplots_adjust(hspace=0.5, wspace=0.)
 
 for label, ax in axs.items():
-    ax.set_title(label, fontfamily='serif', loc='left')
+    ax.set_title(label, fontfamily='serif', loc='left', fontsize=fontsize+2)
 
-ax1 = axs['(a)']
+ax1 = axs['(a) 100kHz']
 for i in range(to_show):
-    ax1.plot(data100_to_plot[i], color='blue', alpha=0.05)
-ax1.set_xlabel('Samples')
-ax1.set_ylabel(r'$uV$')
+    ax1.plot(np.arange(data100_to_plot.shape[1])*20/1000, data100_to_plot[i],alpha=0.05)
+ax1.set_xlabel(r'$\mu s$', fontsize=fontsize)
+ax1.set_ylabel(r'$\mu V$', fontsize=fontsize)
 ax1.set_ylim(-1000, 20000)
-ax1.set_xlim(0, 1000)
-ax1.set_xticks(np.arange(0, 1000, 200))
+ax1.set_xlim(0, 20)
+ax1.set_xticks(np.arange(0, 20, 5))
+ax1.tick_params(labelsize=fontsize-2)
 
-ax2 = axs['(b)']
+ax2 = axs[f'(b) {high_freq}kHz']
 for i in range(to_show):
-    ax2.plot(data_high_to_plot[i], color='blue', alpha=0.05)
-ax2.set_xlabel('Samples')
+    ax2.plot(np.arange(data_high_to_plot.shape[1])*20/1000, data_high_to_plot[i], alpha=0.05)
+ax2.set_xlabel(r'$\mu s$', fontsize=fontsize)
 ax2.set_ylim(-1000, 20000)
 ax2.set_yticks([])
-ax2.set_xlim(0, 2*period)
+ax2.set_xlim(0, 2*period*20/1000)
+ax2.tick_params(labelsize=fontsize-2)
 
-ax3 = axs['(c)']
-ax4 = axs['(d)']
+# ax3 = axs['(c)']
+# ax4 = axs['(d)']
+#
+# refTraces.fit_histogram(plot=True, fig_name=None, coloring=False, indexing=True,
+#                         hist_color='dimgray', plot_fit=False)
+# plt.ylim(0, 320)
+# pos3 = ax3.get_position()
+# ax3.set_position(ax4.get_position())
+# ax4.set_position(pos3)
+#
+# # ax3.hist(refTraces.inner_products(), bins=refTraces.num_bins, color='cyan', alpha=1.)
+# ax3.set_xlabel('Inner product', fontsize=fontsize)
+# ax3.set_ylabel('Counts', fontsize=fontsize)
+# ax3.set_ylim(0, 320)
+# ax3.tick_params(labelsize=fontsize-2)
+#
+# ax4.hist(highTraces.inner_products(), bins=highTraces.num_bins, color='dimgray', alpha=1.)
+# ax4.set_xlabel('Inner product', fontsize=fontsize)
+# ax4.set_ylim(0, 320)
+# ax4.set_yticks([])
+# ax4.tick_params(labelsize=fontsize-2)
 
-refTraces.fit_histogram(plot=True, fig_name=None, coloring=False, indexing=True,
-                        hist_color='blue', plot_fit=False)
-plt.ylim(0, 320)
-pos3 = ax3.get_position()
-ax3.set_position(ax4.get_position())
-ax4.set_position(pos3)
+plt.show()
 
-ax3.hist(refTraces.inner_products(), bins=refTraces.num_bins, color='blue', alpha=1.)
-ax3.set_xlabel('Inner product')
-ax3.set_ylabel('Counts')
-ax3.set_ylim(0, 320)
-
-
-
-ax4.hist(highTraces.inner_products(), bins=highTraces.num_bins, color='blue', alpha=1.)
-ax4.set_xlabel('Inner product')
-ax4.set_ylim(0, 320)
-ax4.set_yticks([])
-
-fig.savefig(DFUtils.create_filename(save_dir + rf'\{sub_dir}_100_vs_{high_freq}kHz_traces.pdf'))
+fig.savefig(DFUtils.create_filename(save_dir + rf'\{sub_dir}_100_vs_{high_freq}kHz_just_traces.pdf'))
--- Scripts/plotting/plot_tvd.py
+++ Scripts/plotting/plot_tvd.py
@@ -12,6 +12,7 @@ def tvd(a, b):
 powers = [5,8,7]
 special_power = 8
 special_reprate = 700
+special_reprate2 = 900
 results_dict = {}
 for power in powers:
     df = pd.read_csv(rf'..\params\RF_results_raw_{power}.csv')
@@ -48,8 +49,10 @@ for i, power in enumerate(powers):
     if power == special_power:
         id_100 = np.argmax(freqs==100)
         id_special = np.argmax(freqs==special_reprate)
+        id_special2 = np.argmax(freqs==special_reprate2)
         ax1.plot(freqs[id_100], tvds[id_100], 'D', color='black', zorder=4, markersize=12)
         ax1.plot(freqs[id_special], tvds[id_special], 'D', color='gray', zorder=4, markersize=12)
+        ax1.plot(freqs[id_special2], tvds[id_special2], 'D', color='saddlebrown', zorder=4, markersize=12)
 
 ax1.legend(loc='upper left', fontsize=fontsize-2)
 ax1.set_xlabel('Repetition rate/kHz', fontsize=fontsize)
@@ -62,21 +65,43 @@ row_100 = df.loc[df['rep_rate']==100, :]
 mu_100 = row_100.loc[0, 'fit_mu']
 pn_100= np.array(row_100.loc[:, '0':].iloc[0])
 pn = np.array(df.loc[df['rep_rate']== special_reprate, '0':].iloc[0])
+pn2 = np.array(df.loc[df['rep_rate']== special_reprate2, '0':].iloc[0])
 labels = np.arange(len(pn_100))
-width=0.4
+width=0.3
 
-ax2.bar(labels - width/2, pn_100, width=width, align='center', alpha=0.8, label='100kHz', color='black')
-ax2.bar(labels + width/2, pn, width=width, align='center', alpha=0.8, label=f'{special_reprate}kHz', color='gray')
-ax2.set_ylim(0, 0.25)
+ax2.bar(labels - width, pn_100, width=width, align='center', alpha=0.8, label='100kHz', color='black')
+ax2.bar(labels, pn, width=width, align='center', alpha=0.8, label=f'{special_reprate}kHz', color='gray')
+ax2.bar(labels + width, pn2, width=width, align='center', alpha=0.8, label=f'{special_reprate2}kHz', color='saddlebrown')
+
+ax2.set_ylim(0, 0.3)
 ax2.set_ylabel('Probability', fontsize=fontsize)
 ax2.set_xlabel('Photon number', fontsize=fontsize)
 ax2.tick_params(labelsize=fontsize-2)
 
 def poisson_norm(x, mu):
     return (mu ** x) * np.exp(-mu) / factorial(x)
-ax2.plot(labels, poisson_norm(labels, mu_100), 'rx-', label=rf'$\mu=${mu_100:.2f}')
+ax2.plot(labels - width, poisson_norm(labels, mu_100), 'rx-', label=rf'$\mu=${mu_100:.2f}')
 
 ax2.legend(loc='upper right', fontsize=fontsize-2)
 plt.show()
 
-fig.savefig(DFUtils.create_filename(r'..\..\Plots\TVD_plots\TVD_plot_sidebyside.pdf'))
+fig.savefig(DFUtils.create_filename(r'..\..\Plots\TVD_plots\TVD_plot_sidebyside_3specials.pdf'))
+
+
+plt.figure('accuracy')
+for power in powers:
+    df = results_dict[power]
+
+    freqs = np.array(df.loc[:, 'rep_rate'])
+    accuracies = np.array(df.loc[:, 'acc_score'])
+    pn_100 = np.array(df.loc[df['rep_rate'] == 100, '0':].iloc[0])
+    labels = np.arange(len(pn_100))
+    mean_photon = np.sum(pn_100 * labels)
+
+    plt.plot(freqs, accuracies, 'o-', label=f'{mean_photon:.2f}', alpha=0.8, markersize=8)
+
+plt.legend(fontsize=fontsize-2)
+plt.ylabel('Test accuracy score', fontsize=fontsize)
+plt.xlabel('Repetition rate/kHz', fontsize=fontsize)
+plt.tick_params(labelsize = fontsize-2)
+plt.savefig(r'..\..\Plots\TVD_plots\RF_accuracy_scores.pdf')
