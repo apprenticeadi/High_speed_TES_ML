4221ce8cca99718b5ccb698ef55b634e4ac72f62
ph-rpatel8-24 <zhenghao.li.1999@gmail.com>
2024-04-12 15:46:34 +0100

    add mean photon number estimation script

--- scripts/run_inner_product_classifier.py
+++ scripts/run_inner_product_classifier.py
@@ -1,19 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.special import factorial
+# import sys
+#
+# sys.path.insert(0, r'F:\TES_python')
 
 from tes_resolver.classifier.inner_product import InnerProductClassifier
 from tes_resolver.traces import Traces
 from src.data_utils import DataReader
 import tes_resolver.config as config
 
-'''Run the inner product classifier'''
 
-dataReader = DataReader('data_20240410')
+'''Data'''
+dataReader = DataReader('Data_2024_04')
 
-data_group = 'raw_1'
+data_group = 'raw_2'
 rep_rate = 100
+file_num = 0
+'''Attenuation calibration'''
+attenuation_db = -66.2
+attenuation_pctg = 10 ** (attenuation_db / 10)
+laser_power = 176.7 / 1e9 / 1.03  #W
+tes_input_power = laser_power * attenuation_pctg  # W
+energy_per_pulse = tes_input_power / (rep_rate * 1000)
+from scipy.constants import h, c
 
-data_raw = dataReader.read_raw_data(data_group, rep_rate)
+wavelength = 1550 * 1e-9
+
+est_mean_ph = energy_per_pulse / (h * c / wavelength)
+print(est_mean_ph)
+
+'''Run the inner product classifier'''
+data_raw = dataReader.read_raw_data(data_group, rep_rate, file_num=file_num)
 calTraces = Traces(rep_rate, data_raw, parse_data=True, trigger=0)
 
 ipClassifier = InnerProductClassifier(multiplier=1., num_bins=1000)
@@ -39,6 +57,15 @@ characeristic_traces = calTraces.characteristic_traces()
 for pn in characeristic_traces.keys():
     plt.plot(characeristic_traces[pn], color='red', alpha=1.)
 
+plt.figure('PN distribution')
+plt.bar(pns, freq)
+
+def poisson_norm(x, mu):
+    return (mu ** x) * np.exp(-mu) / factorial(x)
+
+plt.plot(pns, poisson_norm(pns, mean_photon_number), '-o', color='red', label=f'mean={mean_photon_number}')
+plt.plot(pns, poisson_norm(pns, est_mean_ph), '-o', color='orange', label=f'mean={est_mean_ph}')
+plt.legend()
 #
 # ave_trace = calTraces.average_trace()
 # plt.plot(ave_trace, color='black', alpha=1.)
@@ -46,17 +73,3 @@ for pn in characeristic_traces.keys():
 
 plt.show()
 
-attenuation_db = -87
-attenuation_pctg = 10 ** (attenuation_db / 10)
-
-laser_power = 13.15 / 1e6 / 1.005  #W
-tes_input_power = laser_power * attenuation_pctg  # W
-
-energy_per_pulse = tes_input_power / (rep_rate * 1000)
-
-from scipy.constants import h, c
-
-wavelength = 1550 * 1e-9
-
-est_mean_ph = energy_per_pulse / (h * c / wavelength)
-print(est_mean_ph)
