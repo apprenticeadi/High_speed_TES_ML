ab56095d74c4908c07424c336b54684835663aab
ph-rpatel8-24 <zhenghao.li.1999@gmail.com>
2024-04-23 13:25:01 +0100

    tomo utils to estimate mean photon number

--- scripts/run_inner_product_classifier.py
+++ scripts/run_inner_product_classifier.py
@@ -7,43 +7,40 @@ from tes_resolver.classifier.inner_product import InnerProductClassifier
 from tes_resolver.traces import Traces
 from src.data_reader import DataReader
 import tes_resolver.config as config
-from src.utils import DFUtils
+from src.utils import DFUtils, TomoUtils
 
 '''Data'''
-dataReader = DataReader('Data_2024_04')
+dataReader = DataReader('Tomography_data_2024_04')
 
-data_group = 'raw_21'
-rep_rate = 200
+data_group = 'power_6'
+rep_rate = 500
 file_num = 0
-raw_traces_to_plot = 1000
+raw_traces_to_plot = 100
 
-if rep_rate < 100:
-    parse_data = False
+if rep_rate <= 300:
+    trigger = 0
 else:
-    parse_data = True
-
-save_fig = True
+    trigger = 'automatic'
 
 '''Estimate mean photon number '''
 attenuation_db = -60.16
-attenuation_pctg = 10 ** (attenuation_db / 10)
 bs_ratio = 98.35526316
 bs_ratio_error = 0.73315
-# laser_power = 1842 / 1e9 / 1.0138  #W
-pm_reading = 7.575 * 1e-6  # W
-pm_reading_error = 0.002 * 1e-6
-wavelength = 1550 * 1e-9
+pm_reading = 26.84 * 1e-6  # W
+pm_reading_error = 0.01 * 1e-6
 
-laser_power = pm_reading / bs_ratio  #W
-tes_input_power = laser_power * attenuation_pctg  # W
-energy_per_pulse = tes_input_power / (rep_rate * 1000)
-est_mean_ph = energy_per_pulse / (h * c / wavelength)
-est_error = est_mean_ph * np.sqrt(bs_ratio_error**2/ bs_ratio**2 + pm_reading_error**2/ pm_reading**2)
+# laser_power = pm_reading / bs_ratio  #W
+# tes_input_power = laser_power * attenuation_pctg  # W
+# energy_per_pulse = tes_input_power / (rep_rate * 1000)
+# est_mean_ph = energy_per_pulse / (h * c / wavelength)
+# est_error = est_mean_ph * np.sqrt(bs_ratio_error**2/ bs_ratio**2 + pm_reading_error**2/ pm_reading**2)
+
+est_mean_ph, est_error = TomoUtils.estimate_av_pn(rep_rate, pm_reading, attenuation_db, bs_ratio, pm_error=pm_reading_error, bs_error=bs_ratio_error)
 print(rf'Estimated mean photon number = {est_mean_ph} with error= {est_error}')
 
 '''Run the inner product classifier'''
 data_raw = dataReader.read_raw_data(data_group, rep_rate, file_num=file_num)
-calTraces = Traces(rep_rate, data_raw, parse_data=parse_data, trigger=0)
+calTraces = Traces(rep_rate, data_raw, parse_data=True, trigger=trigger)
 
 ipClassifier = InnerProductClassifier(multiplier=1., num_bins=1000)
 ipClassifier.train(calTraces)
@@ -69,9 +66,9 @@ ax.set_ylabel('Counts')
 ax = axs[1]
 for i in range(raw_traces_to_plot):
     ax.plot(calTraces.data[i], alpha=0.1)
-characeristic_traces = calTraces.characteristic_traces()
-for pn in characeristic_traces.keys():
-    ax.plot(characeristic_traces[pn], color='red', alpha=1., label='Characteristic traces')
+# characeristic_traces = calTraces.characteristic_traces()
+# for pn in characeristic_traces.keys():
+#     ax.plot(characeristic_traces[pn], color='red', alpha=1., label='Characteristic traces')
 ax.set_xlabel('Samples')
 ax.set_title(f'First {raw_traces_to_plot} raw traces')
 
@@ -88,6 +85,10 @@ ax.legend()
 #
 # ave_trace = calTraces.average_trace()
 # plt.plot(ave_trace, color='black', alpha=1.)
+#
+# plt.figure('trace train')
+# plt.plot(calTraces.data[:10, :].flatten())
+# plt.xlabel('Samples')
 
 plt.show()
 
