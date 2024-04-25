ff7a475e691d42164313ceba36fc5705ac7b8dfd
Li <zhenghao.li.1999@gmail.com>
2024-04-25 18:06:43 +0100

    correct trigger delay

--- scripts/run_inner_product_classifier.py
+++ scripts/run_inner_product_classifier.py
@@ -1,3 +1,5 @@
+import time
+
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.special import factorial
@@ -5,6 +7,7 @@ from scipy.constants import h, c
 
 from tes_resolver.classifier.inner_product import InnerProductClassifier
 from tes_resolver.traces import Traces
+from tes_resolver.data_chopper import DataChopper
 from src.data_reader import DataReader
 import tes_resolver.config as config
 from src.utils import DFUtils, TomoUtils
@@ -15,15 +18,10 @@ distribution'''
 '''Data'''
 dataReader = DataReader('Tomography_data_2024_04')
 
-data_group = 'power_11'
-rep_rate = 1000
+data_group = 'power_0'
+rep_rate = 500
 file_num = 0
-raw_traces_to_plot = 10000
-
-if rep_rate <= 300:
-    trigger = 0
-else:
-    trigger = 'automatic'
+raw_traces_to_plot = 1000
 
 '''Estimate mean photon number '''
 attenuation_db = -60.16
@@ -32,25 +30,29 @@ bs_ratio_error = 0.73315
 pm_reading = 8.710 * 1e-6  # W
 pm_reading_error = 0.01 * 1e-6
 
-# laser_power = pm_reading / bs_ratio  #W
-# tes_input_power = laser_power * attenuation_pctg  # W
-# energy_per_pulse = tes_input_power / (rep_rate * 1000)
-# est_mean_ph = energy_per_pulse / (h * c / wavelength)
-# est_error = est_mean_ph * np.sqrt(bs_ratio_error**2/ bs_ratio**2 + pm_reading_error**2/ pm_reading**2)
-
 est_mean_ph, est_error = TomoUtils.estimate_av_pn(rep_rate, pm_reading, attenuation_db, bs_ratio, pm_error=pm_reading_error, bs_error=bs_ratio_error)
 print(rf'Estimated mean photon number = {est_mean_ph} with error= {est_error}')
 
-'''Run the inner product classifier'''
+'''Read data '''
 data_raw = dataReader.read_raw_data(data_group, rep_rate, file_num=file_num)
-calTraces = Traces(rep_rate, data_raw, parse_data=True, trigger=trigger)
 
+if rep_rate <= 300:
+    trigger_delay = 0
+else:
+    trigger_delay = DataChopper.find_trigger(data_raw, samples_per_trace=int(5e4 / rep_rate))
+
+targetTraces = Traces(rep_rate, data_raw, parse_data=True, trigger_delay=0)
+
+'''Run the inner product classifier'''
 ipClassifier = InnerProductClassifier(multiplier=1., num_bins=1000)
-ipClassifier.train(calTraces)
+ipClassifier.train(targetTraces)
 
-labels = ipClassifier.predict(calTraces, update=True)
+t1 = time.time()
+labels = ipClassifier.predict(targetTraces, update=True)
+t2 = time.time()
+print(f'ip classifier predicts {targetTraces.num_traces} in {t2-t1}s')
 
-pns, freq = calTraces.pn_distribution(normalised=True)
+pns, freq = targetTraces.pn_distribution(normalised=True)
 
 mean_photon_number = np.sum(pns * freq)
 print(f'TES reported mean photon number is {mean_photon_number}')
@@ -59,7 +61,7 @@ fig, axs = plt.subplots(1, 3, figsize=(18, 5), layout='constrained')
 
 ax = axs[0]
 ax.set_title('Inner products')
-overlaps = ipClassifier.calc_inner_prod(calTraces)
+overlaps = ipClassifier.calc_inner_prod(targetTraces)
 ax.hist(overlaps, bins=ipClassifier.num_bins, color='aquamarine')
 for pn_bin in ipClassifier.inner_prod_bins.values():
     ax.axvline(pn_bin, ymin=0, ymax=0.5, ls='dashed')
@@ -67,9 +69,9 @@ ax.set_xlabel('Inner product')
 ax.set_ylabel('Counts')
 
 ax = axs[1]
-for i in range(raw_traces_to_plot):
-    ax.plot(calTraces.data[i], alpha=0.1)
-# characeristic_traces = calTraces.characteristic_traces()
+# for i in range(raw_traces_to_plot):
+#     ax.plot(targetTraces.data[i], alpha=0.1)
+# characeristic_traces = targetTraces.characteristic_traces()
 # for pn in characeristic_traces.keys():
 #     ax.plot(characeristic_traces[pn], color='red', alpha=1., label='Characteristic traces')
 ax.set_xlabel('Samples')
@@ -86,11 +88,11 @@ ax.plot(pns, poisson_norm(pns, mean_photon_number), '-o', color='red', label=f'm
 ax.plot(pns, poisson_norm(pns, est_mean_ph), '-o', color='orange', label=f'mean={est_mean_ph}')
 ax.legend()
 #
-# ave_trace = calTraces.average_trace()
+# ave_trace = targetTraces.average_trace()
 # plt.plot(ave_trace, color='black', alpha=1.)
 #
 fig2, ax2 = plt.subplots(figsize=(20, 8))
-ax2.plot(calTraces.data[:100000, :].flatten())
+ax2.plot(targetTraces.data[:100000, :].flatten())
 ax2.set_xlabel('Samples')
 
 plt.show()
