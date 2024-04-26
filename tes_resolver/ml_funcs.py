781cd4161173741c3fa93b7a4bf45fe3f79cd535
Li <zhenghao.li.1999@gmail.com>
2024-04-26 10:45:03 +0100

    generate training traces, allow to specify a trigger delay for the training traces

--- tes_resolver/ml_funcs.py
+++ tes_resolver/ml_funcs.py
@@ -4,7 +4,7 @@ from tes_resolver.traces import Traces
 from tes_resolver.data_chopper import DataChopper
 
 
-def generate_training_traces(calTraces: Traces, target_rep_rate):
+def generate_training_traces(calTraces: Traces, target_rep_rate, trigger_delay=0):
     """Overlap calibration traces to emulate high freuqency data """
     labels = calTraces.labels
     new_period = int(calTraces.sampling_rate / target_rep_rate)
@@ -12,12 +12,12 @@ def generate_training_traces(calTraces: Traces, target_rep_rate):
     filtered_indices = np.where(labels >= 0)[0]  # unclassified traces have default lables -1
     training_labels = labels[filtered_indices]
 
-    training_data = DataChopper.overlap_to_high_freq(calTraces.data, new_period, selected_traces=filtered_indices, reshape=True)
+    training_data = DataChopper.overlap_to_high_freq(calTraces.data, new_period, selected_traces=filtered_indices, reshape=False)
 
-    training_data = training_data[calTraces.period//new_period:]  # remove the first few traces that do not overlap with preceeding tails.
+    training_data = training_data[new_period * (calTraces.period//new_period):]  # remove the first few traces that do not overlap with preceeding tails.
     training_labels = training_labels[calTraces.period//new_period:]
 
-    trainingTraces = Traces(target_rep_rate, training_data, training_labels, calTraces.sampling_rate, parse_data=False)
+    trainingTraces = Traces(target_rep_rate, training_data, training_labels, calTraces.sampling_rate, parse_data=True, trigger_delay=trigger_delay)
 
     return trainingTraces
 
