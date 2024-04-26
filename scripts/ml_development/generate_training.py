a7389926c63de8d55d650a7c6cbac208ddf9b719
Li <zhenghao.li.1999@gmail.com>
2024-04-26 10:42:40 +0100

    remove baseline and overlap to training data

--- scripts/ml_development/generate_training.py
+++ scripts/ml_development/generate_training.py
@@ -13,38 +13,55 @@ from src.data_reader import DataReader
 # maybe also try plotting the raw traces and see how they cluster (training data vs actual data)
 
 '''Parameters'''
-ref_rep_rate = 200
-high_rep_rates = np.arange(300, 1100, 100)
+ref_rep_rate = 100
+high_rep_rates = np.arange(200, 1100, 100)
 raw_traces_to_plot = 1000
 
 sampling_rate = 5e4
-dataReader = DataReader('Tomography_data_2024_04')
-data_group = 'power_6'
+dataReader = DataReader('Data/Tomography_data_2024_04')
+data_group = 'power_0'
 
 '''Inner product classifier'''
 ipClassifier = InnerProductClassifier(multiplier=1., num_bins=1000)
 
-# '''Find the vertical offset'''
-# cal_data_raw = dataReader.read_raw_data(data_group, ref_rep_rate)
-# calTraces = Traces(ref_rep_rate, cal_data_raw, parse_data=True, trigger=0)
-#
-# ipClassifier.train(calTraces)
-# ipClassifier.predict(calTraces, update=True)
-#
-# vertical_offset = calTraces.find_offset()
+'''Read and label the reference data'''
+ref_data = dataReader.read_raw_data(data_group, ref_rep_rate)
+refTraces = Traces(ref_rep_rate, ref_data, parse_data=True, trigger_delay=0)
+
+ipClassifier.train(refTraces)
+ipClassifier.predict(refTraces, update=True)
+
+'''Remove the baseline '''
+# refTraces.pca_cleanup(1)  # pca clean up: bad idea
+baseline_ref = refTraces.find_offset()
+refTraces.data = refTraces.data - baseline_ref  # remove the baseline
+
+plt.figure('reference data', figsize=(12, 5))
+plt.plot(refTraces.data[:10, :].flatten())
+plt.title('reference data')
+for i in range(10):
+    plt.axvline(i * 500, ymin=0, ymax=0.8, color='black', ls='dashed')
+plt.axhline(0, 0, 1, color='red', ls = 'dashed')
+
+plt.figure('more reference data')
+for i in range(raw_traces_to_plot):
+    plt.plot(refTraces.data[i, :], alpha=0.5)
+plt.axhline(0, 0, 1, color='black', ls='dashed', alpha=0.5)
 
 '''Loop over high rep rates'''
 training_traces = {}
 actual_traces = {}
 
-fig1, axs1 = plt.subplot_mosaic(high_rep_rates.reshape((2,4)), figsize=(15, 6), sharex=True, sharey=True, layout='constrained')  # plot average trace
+plot_kwargs = {'figsize':(15, 10), 'sharex':True, 'sharey':True, 'layout':'constrained'}
+
+fig1, axs1 = plt.subplot_mosaic(high_rep_rates.reshape((3,3)), **plot_kwargs)  # plot average trace
 fig1.suptitle('Average trace')
 
-fig2, axs2 = plt.subplot_mosaic(high_rep_rates.reshape((2,4)), figsize=(15, 6), sharex=True, sharey=True, layout='constrained')  # plot raw traces to see clustering
-fig2.suptitle('Raw training traces')
+fig2, axs2 = plt.subplot_mosaic(high_rep_rates.reshape((3,3)), **plot_kwargs)  # plot raw traces to see clustering
+fig2.suptitle('Training traces')
 
-fig3, axs3 = plt.subplot_mosaic(high_rep_rates.reshape((2,4)), figsize=(15, 6), sharex=True, sharey=True, layout='constrained')  # plot raw traces to see clustering
-fig3.suptitle('Raw actual traces')
+fig3, axs3 = plt.subplot_mosaic(high_rep_rates.reshape((3,3)), **plot_kwargs)  # plot raw traces to see clustering
+fig3.suptitle('Actual traces')
 for high_rep_rate in high_rep_rates:
     ax1 = axs1[high_rep_rate]
     ax2 = axs2[high_rep_rate]
@@ -61,20 +78,16 @@ for high_rep_rate in high_rep_rates:
     actualTraces = Traces(high_rep_rate, actual_data, parse_data=True, trigger_delay=trigger_delay)
     actual_traces[high_rep_rate] = actualTraces
 
-    '''Read reference traces with the correct trigger'''
-    ref_data = dataReader.read_raw_data(data_group, ref_rep_rate)
-    refTraces = Traces(ref_rep_rate, ref_data, parse_data=True, trigger_delay=trigger_delay)
+    '''Generate training '''
+    trainingTraces = generate_training_traces(refTraces, high_rep_rate, trigger_delay=trigger_delay)
 
-    '''Label reference traces'''
-    ipClassifier.train(refTraces)  # retrain the ip classifier
-    ipClassifier.predict(refTraces, update=True)
-    vertical_offset = refTraces.find_offset()  # this is the average value of zero-photon traces
+    # for high overlap data, zero the zero-photon characteristic trace in training data.
+    if high_rep_rate >= 600:
+        zero_of_the_zero = np.min(np.mean(trainingTraces.pn_traces(0), axis=0))
+        if zero_of_the_zero >0:
+            trainingTraces.data = trainingTraces.data - zero_of_the_zero
 
-    '''Generate training data'''
-    trainingTraces = generate_training_traces(refTraces, high_rep_rate)
-
-    num_tails = int(high_rep_rate / ref_rep_rate + 0.5) - 1
-    trainingTraces.data = trainingTraces.data - num_tails * vertical_offset  # subtract the appropriate number of offsets.
+    # trainingTraces.data = trainingTraces.data + baseline_ref  # add back the baseline
     training_traces[high_rep_rate] = trainingTraces
 
     '''Plot average traces'''
@@ -83,12 +96,19 @@ for high_rep_rate in high_rep_rates:
     ax1.plot(actualTraces.average_trace(), label='Actual')
     ax1.set_xlabel('Samples')
 
-    '''Plot raw traces to see clustering'''
+    '''Plot training traces'''
     ax2.set_title(f'{high_rep_rate}kHz')
     for i in range(raw_traces_to_plot):
         ax2.plot(trainingTraces.data[i], alpha=0.1)
+
+    # plot characteristic traces
+    char_trace_dict = trainingTraces.characteristic_traces()
+    for pn in char_trace_dict.keys():
+        ax2.plot(char_trace_dict[pn], color='red')
+
     ax2.set_xlabel('Samples')
 
+    '''Plot actual traces'''
     ax3.set_title(f'{high_rep_rate}kHz')
     for i in range(raw_traces_to_plot):
         ax3.plot(actualTraces.data[i], alpha=0.1)
