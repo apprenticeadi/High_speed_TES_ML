096653bbc28bea94cd56df52a28c8665bf49ad3c
Li <zhenghao.li.1999@gmail.com>
2024-05-24 16:03:27 +0100

    plot calibration with error bar

--- scripts/plotting/plot_calibration.py
+++ scripts/plotting/plot_calibration.py
@@ -5,55 +5,63 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import string
 from scipy.optimize import curve_fit
-from scipy.stats import bootstrap
 from src.utils import poisson_norm
+
+import scipy.odr as odr
 
 '''Compares the power meter calibrated photon number distribution with what the TES reports at 100kHz '''
 
 powers = np.arange(12)
 modeltype='IP'
-bar_width= 0.4
+bar_width= 0.8
+rep_rate = 100
+
+pm_mean_error = 0.046
 
 alphabet = list(string.ascii_lowercase)
 
 params_dir = rf'..\..\Results\Tomography_data_2024_04\Params'
 log_df = pd.read_csv(params_dir + r'\log_2024_04_22.csv')
-result_time_stamp = r'2024-05-15(19-27-44.036789)'
 
+'''Plot every single power distribution compared with PM Poissonian distribution'''
 fig1, axs1 = plt.subplot_mosaic(powers.reshape((4, 3)), sharex=True, sharey=True, figsize=(15, 10), layout='constrained')  # plot 100kHz pn distribution
 
 pm_mean_pns = np.zeros(len(powers))
 tes_mean_pns = np.zeros((len(powers), 3))  # with negative and postive errors
 for i_power, power in enumerate(powers):
 
-    # set up plot
+    # Set up plot
     ax1 = axs1[power]
     ax1.set_title(f'({alphabet[i_power]}) power_{power}', loc='left')
 
-    # TES result
-    params_df = pd.read_csv(params_dir + rf'\{modeltype}\{modeltype}_results_power_{power}.csv')
-    tes_distrib = list(params_df.loc[params_df['rep_rate']==100, '0':].iloc[0])
+    # TES result with errors
+    distrib_df = pd.read_csv(params_dir + rf'\{modeltype}\{modeltype}_results_power_{power}.csv')
+    tes_distrib = np.array(distrib_df.loc[distrib_df['rep_rate'] == rep_rate, '0':].iloc[0])
+    tes_distrib = np.nan_to_num(tes_distrib)
+
     labels = np.arange(len(tes_distrib))
 
-    # bootstrap for mean photon number
-    raw_labels = np.load(
-        params_dir + rf'\..\{modeltype}\power_{power}_{result_time_stamp}\{modeltype}_power_{power}_100kHz_raw_labels.npy')
-    print(f'Start bootstrapping for power_{power}')
-    t1 = time.time()
-    tes_mean = np.mean(raw_labels)
-    btstrp_res = bootstrap((raw_labels,), np.mean, confidence_level=0.95, batch=100, method='basic')
-    t2 = time.time()
-    print(f'Finish after {t2 - t1}s')
-    tes_mean_pns[i_power] = [tes_mean, tes_mean - btstrp_res.confidence_interval.low,
-                             btstrp_res.confidence_interval.high - tes_mean]
+    tes_distrib_errors = np.zeros((2, len(tes_distrib)))  # first row negative, second row positive
+    for i_row, n_or_p in enumerate(['n', 'p']):
+        errors_df = pd.read_csv(params_dir + rf'\{modeltype}\bootstrapped\{modeltype}_results_power_{power}_{n_or_p}_error.csv')
+        tes_distrib_errors[i_row] = np.array(errors_df.loc[errors_df['rep_rate'] == rep_rate, '0':].iloc[0])
+    tes_distrib_errors = np.nan_to_num(tes_distrib_errors)
 
-    # power meter calibration result
+    # TES reported mean photon number and error
+    mean_df = pd.read_csv(params_dir+rf'\{modeltype}\bootstrapped_means\{modeltype}_results_power_{power}.csv')
+    tes_mean_pns[i_power] = np.array(mean_df.loc[mean_df['rep_rate'] == rep_rate, ['mean_pn', 'n_error', 'p_error']].iloc[0])
+
+    # power meter calibration result with errors
     pm_mu =  log_df.loc[(log_df['power_group'] == f'power_{power}') & (log_df['rep_rate/kHz'] == 100), 'pm_estimated_av_pn'].iloc[0]
     pm_mean_pns[i_power] = pm_mu
     pm_distrib = poisson_norm(labels, pm_mu)
 
-    ax1.bar(labels - bar_width/2, pm_distrib, width=bar_width, align='center', label='Input')
-    ax1.bar(labels + bar_width / 2, tes_distrib, width=bar_width, align='center', label='Measured')
+    pm_distrib_errors = np.abs(labels - pm_mu) * pm_mean_error * pm_distrib
+
+    ax1.bar(labels, tes_distrib, width=bar_width, align='center', label='Measured',
+            yerr = tes_distrib_errors, capsize=5, error_kw={'barsabove':True, 'elinewidth':10},
+            color='blue', alpha=0.7)
+    ax1.errorbar(labels, pm_distrib, yerr=pm_distrib_errors, fmt='.--', label='Input', color='red')
 
     if i_power == 0 :
         ax1.legend()
@@ -65,16 +73,20 @@ for i_power, power in enumerate(powers):
 fig2, ax2 = plt.subplots()  # plot input vs measured mean photon number
 
 ax2.set_title('Detector efficiency')
-pm_errors = pm_mean_pns * 0.058
-ax2.errorbar(pm_mean_pns, tes_mean_pns[:, 0], xerr=pm_errors, yerr=tes_mean_pns[:, 1:].T, fmt= 'o', ls='None')
+pm_errors = pm_mean_pns * pm_mean_error
+ax2.errorbar(pm_mean_pns, tes_mean_pns[:, 0], xerr=pm_errors, yerr=tes_mean_pns[:, 1:].T, fmt= '.', ls='None', label='Data')
 ax2.set_xlabel('Input average photon number (power meter)')
-ax2.set_ylabel('Measured average photon number (TES 100kHz)')
+ax2.set_ylabel(f'Measured average photon number (TES {rep_rate}kHz)')
 
-def loss(x, eta):
-    return eta * x
+def fit(B, x):
+    return B[0] * x
 
-popt, pcov = curve_fit(loss, pm_mean_pns, tes_mean_pns[:, 0], p0=[0.9])
+linear = odr.Model(fit)
+mydata = odr.Data(pm_mean_pns, tes_mean_pns[:, 0], wd=pm_errors, we=np.mean(tes_mean_pns[:, 1:], axis=1))
+myodr = odr.ODR(mydata, linear, beta0=[0.93])
+myoutput = myodr.run()
 
-ax2.plot(pm_mean_pns, popt[0] * pm_mean_pns, ls='dashed')
+ax2.plot(pm_mean_pns, myoutput.beta[0] * pm_mean_pns, ls='dashed', label='Fit')
+ax2.legend()
 
 plt.show()
